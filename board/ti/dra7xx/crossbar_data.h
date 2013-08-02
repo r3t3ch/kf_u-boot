cb56013c39dc495ac654166586f7dd956c8ccb95
Misael Lopez Cruz <misael.lopez@ti.com>
2013-08-02 13:02:54 -0500

    TEMP: ARM: DRA7xx: crossbar: McASP DMA and IRQ data
    McASP3 DMA/IRQ crossbar information is fixed and McASP6 added.
    
    Signed-off-by: Misael Lopez Cruz <misael.lopez@ti.com>

--- board/ti/dra7xx/crossbar_data.h
+++ board/ti/dra7xx/crossbar_data.h
@@ -30,8 +30,10 @@
 const struct crossbar_entry mpu_irq_map[] = {
 	/* MPU_IRQ, CROSSBAR_IRQ */
 	{159, 217},	/* RTC_IRQ */
-	{158, 150},	/* MCASP3_IRQ_AREVT */
-	{157, 151},	/* MCASP3_IRQ_AXEVT */
+	{155, 150},	/* MCASP3_IRQ_AREVT */
+	{154, 151},	/* MCASP3_IRQ_AXEVT */
+	{156, 156},	/* MCASP6_IRQ_AREVT */
+	{157, 157},	/* MCASP6_IRQ_AXEVT */
 	{136, 251},	/* MAILBOX5 */
 	{141, 255},	/* MAILBOX6 */
 	{142, 396},	/* IPU1 MMU */
@@ -42,8 +44,10 @@ const struct crossbar_entry mpu_irq_map[] = {
 
 const struct crossbar_entry sdma_dreq_map[] = {
 	/* SDMA_DREQ, CROSSBAR_DMA */
-	{126, 132},	/* MCASP3_DREQ_RX */
-	{125, 133},	/* MCASP3_DREQ_TX */
+	{79, 132},	/* MCASP3_DREQ_RX */
+	{78, 133},	/* MCASP3_DREQ_TX */
+	{63, 138},	/* MCASP6_DREQ_RX */
+	{62, 139},	/* MCASP6_DREQ_TX */
 };
 
 #endif /* _CROSSBAR_DATA_DRA7XX_H_ */
