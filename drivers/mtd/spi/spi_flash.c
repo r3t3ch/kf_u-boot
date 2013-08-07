e4f64191bce2d63ecc1536392b578992d51b9393
Jagannadha Sutradharudu Teki <jaganna@xilinx.com>
2013-08-07 18:06:59 +0530

    sf: Remove spi_flash_cmd_poll_bit()
    There is no other call other than spi_flash_cmd_wait_ready(),
    hence removed spi_flash_cmd_poll_bit and use the poll status code
    spi_flash_cmd_wait_ready() itself.
    
    Change-Id: I34ea6b0fb847f45636eede7317223a047b5784f5
    Signed-off-by: Jagannadha Sutradharudu Teki <jaganna@xilinx.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    [Added change-id for gerrit]
    Signed-off-by: Amarinder Bindra <a-bindra@ti.com>

--- drivers/mtd/spi/spi_flash.c
+++ drivers/mtd/spi/spi_flash.c
@@ -206,13 +206,14 @@ int spi_flash_cmd_read_fast(struct spi_flash *flash, u32 offset,
 	return ret;
 }
 
-int spi_flash_cmd_poll_bit(struct spi_flash *flash, unsigned long timeout,
-			   u8 cmd, u8 poll_bit)
+int spi_flash_cmd_wait_ready(struct spi_flash *flash, unsigned long timeout)
 {
 	struct spi_slave *spi = flash->spi;
 	unsigned long timebase;
 	int ret;
 	u8 status;
+	u8 poll_bit = STATUS_WIP;
+	u8 cmd = CMD_READ_STATUS;
 
 	ret = spi_xfer(spi, 8, &cmd, NULL, SPI_XFER_BEGIN);
 	if (ret) {
@@ -241,12 +242,6 @@ int spi_flash_cmd_poll_bit(struct spi_flash *flash, unsigned long timeout,
 	/* Timed out */
 	debug("SF: time out!\n");
 	return -1;
-}
-
-int spi_flash_cmd_wait_ready(struct spi_flash *flash, unsigned long timeout)
-{
-	return spi_flash_cmd_poll_bit(flash, timeout,
-		CMD_READ_STATUS, STATUS_WIP);
 }
 
 int spi_flash_cmd_erase(struct spi_flash *flash, u32 offset, size_t len)
