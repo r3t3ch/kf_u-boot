afd2d14dc229a477c6e1031e5e2271b519adef60
Hashcode <hashcode0f@gmail.com>
2014-02-21 14:40:17 -0800

    kc1: update debug functions

--- board/ti/omap4_kc1/kc1_debug.h
+++ board/ti/omap4_kc1/kc1_debug.h
@@ -1,8 +1,14 @@
 #ifndef __KC1_DEBUG_H
 #define __KC1_DEBUG_H
 
-void dump_dss(void);
-void dump_gpio(void);
+void dump_gpio(unsigned int base);
 void dump_prcm(void);
+void dump_dss(void);
+void dump_musb(void);
+void dump_control_general_core(void);
+void dump_control_general_wkup(void);
+void dump_control_padconf_core(void);
+void dump_control_padconf_wkup(void);
+void dump_mcspi(unsigned int base);
 
 #endif /* __KC1_BOARD_H */
